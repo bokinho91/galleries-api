30743fd053279f4478256ae7f8d8f919942f9b8b
bokinho91 <boris.vujakovic@gmail.com>
2022-04-18 12:32:54 +0200

    dodat AuthController, request validation prilikom registrovanja korisnika, i dodata ruga. Takodje integrisan deo za jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\RegisterUserRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function register(RegisterUserRequest $request)
+    {
+        $data = $request->validated();
+
+        return $data;
+    }
+
+
+        /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+
+}
